9222b682d72bd1047f3ebf934fa54e9c04dc1e50
songzhukai <songzhukai@le.com>
2020-07-15 17:45:23 +0800

    update

--- job_manager.py
+++ job_manager.py
@@ -6,16 +6,17 @@ import job.goole_search_job as goole_search
 mongo_user = os.environ.get('Mongo_User', '')
 mongo_pwd = os.environ.get('Mongo_Pwd', '')
 client = pymongo.MongoClient(
-    "mongodb://" + mongo_user + ":" + mongo_pwd + "@cluster0-shard-00-00-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-01-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-02-sefrw.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    "mongodb://" + mongo_user + ":" + mongo_pwd + "@cluster0-shard-00-00-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-01-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-02-sefrw.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    , connect=False)
+
 
 def start(_callbackLineMsg):
     db = client.kentDB
     coll = db['jobs']
-    #找状态为0的job
+    # 找状态为0的job
     myquery = {"status": {"$eq": "0"}}
     for dict in coll.find(myquery):
         goole_search.execute(coll, dict, _callbackLineMsg)
         print(dict)
         # 休息0.5秒
         time.sleep(0.5)
-
--- storage.py
+++ storage.py
@@ -14,7 +14,8 @@ mongo_pwd = os.environ.get('Mongo_Pwd', '')
 
 if mongo_user != '':
     client = pymongo.MongoClient(
-    "mongodb://" + mongo_user + ":" + mongo_pwd + "@cluster0-shard-00-00-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-01-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-02-sefrw.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+        "mongodb://" + mongo_user + ":" + mongo_pwd + "@cluster0-shard-00-00-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-01-sefrw.gcp.mongodb.net:27017,cluster0-shard-00-02-sefrw.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+        , connect=False)
 
 
 def isStockHistortyExists(code):
